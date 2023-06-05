b0200aa5e2e1678a0b66177a6ceb95cac57aa438
lenovo <mahamzahid333@gmail.com>
2023-06-05 11:01:31 -0700

    Quran app

---
+++ app/src/main/java/com/example/myquranapp/Surahcontent.java
@@ -0,0 +1,8 @@
+package com.example.myquranapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+        import android.os.Bundle;
+
+public class Surahcontent extends Activity {
+
+}
